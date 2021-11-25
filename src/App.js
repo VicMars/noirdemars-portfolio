521489c2622a66ed6a41c19f673e6e869ac30ee7
Vicky Andre <59230296+VicMars@users.noreply.github.com>
2021-11-25 10:02:44 -0400

    og:image

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ function App() {
         <title>Vicky André • Portfolio</title>
         <meta name="description" content="The portfolio of Vicky André: a multi-disciplinary designer playing with shapes.
            From Textile to Web Design."/>
-        <meta name="og:image" content="https://noir-de-mars.com/assets/images/Vicky_Andre_portfolio.jpg"/>
+        <meta name="og:image" content="https://noir-de-mars.com/Vicky_Andre_portfolio.jpg"/>
       </Helmet>
 
       <div id="header">
